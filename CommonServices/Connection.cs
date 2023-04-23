137473485185596909ff2d7048e2041a9c7f919c
Your Name <you@example.com>
2023-04-24 00:42:28 +0500

    Restructered the solution

---
+++ CommonServices/Connection.cs
@@ -0,0 +1,30 @@
+﻿using Npgsql;
+using POSGresApi.Settings;
+using System.Data;
+
+namespace POSGresApi.CommonServices
+{
+    public class Connection
+    {
+
+        private static NpgsqlConnection? connection { get; set; }
+
+        public static async Task<IDbConnection> GetConnection()
+        {
+            string? connectionString = AppSettings.ConnectionString;
+
+            if (isConnectionOpen(connection))
+            {
+                return connection;
+            }
+
+            connection = new NpgsqlConnection(connectionString);
+            return connection;
+        }
+
+        public static bool isConnectionOpen(NpgsqlConnection Connection)
+        {
+            return Connection != null && (connection.State == ConnectionState.Open || Connection.State == ConnectionState.Connecting);
+        }
+    }
+}
